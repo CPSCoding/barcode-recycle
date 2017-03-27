5e43c6d8bc6f48002245fd5950bdc582ab9f2cb0
OoTTiiK <8.iii.lll.iii.l.iii.lll.iii.8@gmail.com>
2017-03-26 21:55:00 -0700

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -1,8 +1,8 @@
 import binascii
 import re
 
-ciphertext = '029abcdea0394819abcde123abcde d' #raw_input("Enter ciphertext: ")
-key = 'boat' #raw_input("Enter key: ")
+ciphertext = raw_input("Enter ciphertext: ")
+key = raw_input("Enter key: ")
 
 ciphertextArray = []
 ciphertextValues = []
@@ -14,16 +14,13 @@ secondarray = []
 for index in firstarray:
     secondarray.append(index + 5)
 
-print secondarray
 
 for i in ciphertext:
     ciphertextwords.append(i)
 
-#print ciphertextwords.remove[secondarray[0] - 5]
 badcounter = 0
 badbad = 0
 for i in secondarray:
-    print ciphertextwords
     del ciphertextwords[secondarray[badcounter] - (badbad * 5) - 1]
     del ciphertextwords[secondarray[badcounter] - (badbad * 5) - 2]
     del ciphertextwords[secondarray[badcounter] - (badbad * 5) - 3]
@@ -32,45 +29,23 @@ for i in secondarray:
     badcounter += 1
     badbad += 1
 
-print ciphertextwords
-
-for index in secondarray:
-    letter = ciphertext[index]
-    print int(bin(int(binascii.hexlify(letter), 16)),2) - 32
-
-
 
 #convert ciphertext to binary
-for letter in ciphertext:
+for letter in ciphertextwords:
     ciphertextArray.append(bin(int(binascii.hexlify(letter), 16)))
 
 #converts ciphertext to ascii
 for item in ciphertextArray:
-    ciphertextValues.append(int(item,2))
-
-print ciphertextValues
+    ciphertextValues.append(int(item,2) + 19)
 
 
-'''for char in ciphertext:
-    if 'abcde' in ciphertext:
-        print "it works"
-        print ciphertext.index('e')
-for char in ciphertext:
-    if char == "a":
-        print ciphertext.index(char)
-        #if ciphertext[ciphertext.index(char + 1)] == "b":
-        #    print "success"
+c = 1
+for num in secondarray:
+    ciphertextValues[num - 5 * c] -= 32
+    c += 1
 
 
-#array of indexs in string that have "abcde" one that changes is +5
-firstarray = [m.start() for m in re.finditer('abcde', ciphertext)]
-secondarray=[]
-for index in firstarray:
-    secondarray.append(index + 5)
-
-print secondarray'''
-
-
+#Beginning of key
 keyArray = []
 threekeyArray = []
 keyValues=[]
@@ -98,12 +73,10 @@ for array in threekeyArray:
     keyValues.append(total)
 
 #print "key values: %s" % keyValues
-
-
 keylength = len(key)
-ciphertextlength = len(ciphertext)
+ciphertextlength = len(ciphertextArray)
 
-plaintextArray = []
+plainArray = []
 keycounter = 0
 ciphertextCounter = 0
 
@@ -117,7 +90,14 @@ for i in ciphertextValues:
         j = int(j)
         keycounter += 1
         ciphertextCounter += 1
-        plaintextArray.append(popo + j + 19)
+        plainArray.append(popo - j)
 
-for i in plaintextArray:
+for i in plainArray:
     i = int(i)
+
+#prints the ciphertext
+plainString = ""
+for char in plainArray:
+        plainString += chr(char)
+
+print ("Plaintext: %s" %plainString)
